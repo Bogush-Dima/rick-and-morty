0000a3c502e7dfc4e4786236d1ca05d777dd37d2
Bogush-Dima <68421669+Bogush-Dima@users.noreply.github.com>
2021-01-28 11:21:41 +0300

    feat: add prev/next buttons to Characters

--- src/components/Characters/Person/index.jsx
+++ src/components/Characters/Person/index.jsx
@@ -1,38 +1,41 @@
 import styles from "components/Characters/Person/Person.module.css";
 
-export const Person = () => {
+export const Person = ({ person }) => {
   return (
     <div className={styles.person}>
       <a className={styles.imgLink} href="personCard">
         <img
           className={styles.img}
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          src={person.image}
           alt="img"
         />
       </a>
       <div className={styles.info}>
         <a className={styles.name} href="personCard">
-          Name
+          {person.name}
         </a>
         <p className={styles.status}>
-          <a className={`${styles.exist} ${styles.alive}`} href="filteredByThis">
-            Alive
+          <a
+            className={`${styles.exist} ${styles.alive}`}
+            href="filteredByThis"
+          >
+            {person.status}
           </a>
           <a className={styles.race} href="filteredByThis">
-            Human
+            {person.species}
           </a>
 
           <a className={styles.gender} href="filteredByThis">
-            Male
+            {person.gender}
           </a>
         </p>
         <div>
           <p className={styles.locationTitle}>Last known location:</p>
-          <p className={styles.lastLocation}>Minsk</p>
+          <p className={styles.lastLocation}>{person.location.name}</p>
         </div>
         <div>
           <p className={styles.locationTitle}>First seen in:</p>
-          <p className={styles.FirstSeen}>Gorodeya</p>
+          <p className={styles.FirstSeen}>{person.origin.name}</p>
         </div>
       </div>
     </div>
--- src/components/Characters/index.jsx
+++ src/components/Characters/index.jsx
@@ -1,13 +1,20 @@
-import styles from 'components/Characters/Characters.module.css'
-import { Person } from './Person'
+import styles from "components/Characters/Characters.module.css";
+import { useSelector } from "react-redux";
+import { Person } from "./Person";
 
 export const Characters = () => {
+  const persons = useSelector((store) => store.characters.items);
   return (
     <section className={styles.section}>
-      <Person />
-      <Person />
-      <Person />
-      <Person />
+      <div className={styles.characters}>
+        {persons.map((person) => (
+          <Person person={person} />
+        ))}
+      </div>
+      <div className={styles.btns}>
+        <button className={styles.charactersBtn}>&lt;</button>
+        <button className={styles.charactersBtn}>&gt;</button>
+      </div>
     </section>
-  )
-}
+  );
+};
